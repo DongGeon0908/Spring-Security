7030145f87c46633f181af5abe23d624508642be
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-11-13 21:56:57 +0900

    password Encoder 수정

--- src/main/java/com/security/basic/config/SecurityConfig.java
+++ src/main/java/com/security/basic/config/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.security.basic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new DelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = getPasswordEncoder().encode("ADMIN123");
+        auth.inMemoryAuthentication().withUser("ADMIN").password(password).roles("ADMIN");
+    }
+
     @Override
     public void configure(WebSecurity web) {
         // filtering을 무시함
